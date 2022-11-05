03a975bb1d257a68e9022f4a240fe44fc4ab751d
anshuopinion <anshusopinion@gmail.com>
2022-11-05 21:32:00 +0530

    theme customization done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: "0.75rem", //"12px"
+        base: ".875rem", // 14px
+        md: "1rem", // 16px
+        lg: "1.25rem", // 20px
+        xl: "1.5rem", // 24px
+        "2xl": "1.75rem ", // 28px
+        "3xl": "2.25rem", // 36px
+      },
+    },
   },
   plugins: [],
 };
